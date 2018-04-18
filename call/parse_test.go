9bb157ee7896765c3f783c6ad97778b3e6dab2fe
arthur silva <arxdsilva@gmail.com>
2018-04-18 18:34:18 -0300

    call/parse_test: rmv Attempts type convertions

--- call/parse_test.go
+++ call/parse_test.go
@@ -1,16 +1,17 @@
 package call
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBuildCallWithValidParams(test *testing.T) {
 	params := []string{"http://www.dummy.com", "30"}
 	cl, _ := BuildCall(params, 50, 10)
 
-	assert.Equal(test, URL(params[0]), cl.URL)
-	assert.Equal(test, Attempts(30), cl.Attempts)
+	assert.Equal(test, params[0], cl.URL)
+	assert.Equal(test, 30, cl.Attempts)
 }
 
 func TestBuildCallWithoutParams(test *testing.T) {
@@ -30,39 +31,39 @@ func TestParseAttempts(test *testing.T) {
 	params := []string{"http://www.dummy.com", "10"}
 	attempts, _ := ParseAttempts(params, 50)
 
-	assert.Equal(test, Attempts(10), attempts)
+	assert.Equal(test, 10, attempts)
 }
 
 func TestParseAttemptsWithoutAttempts(test *testing.T) {
 	params := []string{"http://www.dummy.com"}
 	attempts, _ := ParseAttempts(params, 50)
-	assert.Equal(test, Attempts(50), attempts)
+	assert.Equal(test, 50, attempts)
 }
 
 func TestParseAttemptsWithInvalidFormat(test *testing.T) {
 	params := []string{"http://www.dummy.com", "dummyAttempts"}
 	attempts, _ := ParseAttempts(params, 50)
 
-	assert.Equal(test, Attempts(50), attempts)
+	assert.Equal(test, 50, attempts)
 }
 
 func TestBuildCallWithConcurrentCalls(test *testing.T) {
 	params := []string{"http://www.dummy.com", "30", "50"}
 	call, _ := BuildCall(params, 100, 200)
 
-	assert.Equal(test, URL(params[0]), call.URL)
-	assert.Equal(test, Attempts(30), call.Attempts)
-	assert.Equal(test, Attempts(50), call.ConcurrentAttempts)
+	assert.Equal(test, params[0], call.URL)
+	assert.Equal(test, 30, call.Attempts)
+	assert.Equal(test, 50, call.ConcurrentAttempts)
 }
 
 func TestParseConcurrentCallsWithoutConcurrentParameter(test *testing.T) {
 	params := []string{"http://www.dummy.com"}
 	concurrentCalls, _ := ParseConcurrentAttempts(params, 100)
-	assert.Equal(test, Attempts(100), concurrentCalls)
+	assert.Equal(test, 100, concurrentCalls)
 }
 
 func TestParseConcurrentCallsWithInValidFormat(test *testing.T) {
 	params := []string{"http://www.dummy.com", "10", "dummyConcurrentCalls"}
 	concurrentCalls, _ := ParseConcurrentAttempts(params, 100)
-	assert.Equal(test, Attempts(100), concurrentCalls)
+	assert.Equal(test, 100, concurrentCalls)
 }
