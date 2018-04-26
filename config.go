58b0a8f22f237ac12cb781f324b5d29b206d127a
arthur silva <arxdsilva@gmail.com>
2018-04-26 13:37:56 -0300

    config: add getConfig

--- config.go
+++ config.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 // Config is the structure that defines how the user should
 // define the config.json file to make custom requests
 type Config struct {
@@ -11,4 +16,12 @@ type Config struct {
 	Host     string
 	Form     string
 	PostForm map[string][]string
+}
+
+func getConfig() (c []Config, err error) {
+	raw, err := ioutil.ReadFile("./callit.config")
+	if err != nil {
+		return
+	}
+	return c, json.Unmarshal(raw, &c)
 }
