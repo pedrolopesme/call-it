d9be4ab9c3caddcbfb8e1c49126ec283582a1591
arthur silva <arxdsilva@gmail.com>
2018-04-27 20:59:26 -0300

    config: add json bindings

--- config.go
+++ config.go
@@ -8,14 +8,14 @@ import (
 // Config is the structure that defines how the user should
 // define the config.json file to make custom requests
 type Config struct {
-	Name     string
-	Method   string
-	URL      string
-	Body     string
-	Header   map[string][]string
-	Host     string
-	Form     string
-	PostForm map[string][]string
+	Name     string              `json:"name"`
+	Method   string              `json:"method"`
+	URL      string              `json:"url"`
+	Body     string              `json:"body"`
+	Header   map[string][]string `json:"header"`
+	Host     string              `json:"host"`
+	Form     string              `json:"form"`
+	PostForm map[string][]string `json:"postform"`
 }
 
 func getConfig() (c []Config, err error) {
