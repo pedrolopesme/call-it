3598e991752acd961678674e5fb3b2604d547997
arthur silva <arxdsilva@gmail.com>
2018-04-27 21:34:26 -0300

    refactor config

--- config.go
+++ config.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
 // Config is the structure that defines how the user should
@@ -19,9 +21,23 @@ type Config struct {
 }
 
 func getConfig() (c []Config, err error) {
-	raw, err := ioutil.ReadFile("./callit.json")
+	dir, err := os.Open(".")
 	if err != nil {
 		return
 	}
-	return c, json.Unmarshal(raw, &c)
+	files, err := dir.Readdir(0)
+	if err != nil {
+		return
+	}
+	for _, f := range files {
+		if f.Name() != "callit.json" {
+			continue
+		}
+		raw, errRaw := ioutil.ReadFile(f.Name())
+		if err != nil {
+			return c, errRaw
+		}
+		return c, json.Unmarshal(raw, &c)
+	}
+	return c, errors.New("Could not find file callit.json")
 }
