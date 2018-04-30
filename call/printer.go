405d3f05e528a6765d9157135f86374f28ec1f1c
Pedro Mendes <pedrolopesme@gmail.com>
2018-04-30 09:54:14 -0300

    call/printer: formatting output.
    related to #6

--- call/printer.go
+++ call/printer.go
@@ -8,31 +8,32 @@ import (
 	"fmt"
 )
 
-// Print results formatted by Status
+// Print results accord to spec in
+// github.com/pedrolopesme/call-it/issues/6
 func PrintResults(result Result) {
-	data := [][]string{}
-	for statusCode, times := range result.status {
-		data = append(data, []string{strconv.Itoa(statusCode), strconv.Itoa(times)})
-	}
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Status Code", "Times"})
+	table.SetHeader([]string{"URL", "STATUS", "TIMES", "ELAPSED", "AVG", "MIN", "MAX", "TOTAL AVG"})
 	table.SetAutoFormatHeaders(false)
-	for _, v := range data {
-		table.Append(v)
-	}
+
 	totalExecution := formatTime(result.totalExecution)
 	avgExecution := formatTime(result.avgExecution)
 	minExeuction := formatTime(result.minExecution)
 	maxExecution := formatTime(result.maxExecution)
 
-	table.Append([]string{"-------------------------", "-------------------------"})
-	table.Append([]string{"Avg", avgExecution})
-	table.Append([]string{"Min", minExeuction})
-	table.Append([]string{"Max", maxExecution})
-	table.SetFooter([]string{"Total Execution", totalExecution})
+	firstLine := true
+	for statusCode, times := range result.status {
+		if firstLine {
+			table.Append([]string{result.URL.String(), strconv.Itoa(statusCode), strconv.Itoa(times), " ", " ", minExeuction, maxExecution, avgExecution})
+			firstLine = false
+		} else {
+			table.Append([]string{"", strconv.Itoa(statusCode), strconv.Itoa(times), " ", " ", " ", " ", " "})
+		}
+	}
 
+	table.SetFooter([]string{"ELAPSED " + totalExecution, "", "", "", "", "", "", " "})
 	table.Render()
 }
+
 
 func formatTime(time float64) (output string){
 	output = fmt.Sprintf("%.2f", time) + "s"
