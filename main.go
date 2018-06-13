2df541147b78086c08210abcb0c5de2f979ee73c
arthur silva <arxdsilva@gmail.com>
2018-06-13 17:26:09 -0300

    main: add config file support

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/pedrolopesme/call-it/call"
-	"github.com/urfave/cli"
 	"log"
 	"os"
+
+	"github.com/pedrolopesme/call-it/call"
+	"github.com/urfave/cli"
 )
 
 const (
@@ -19,21 +19,41 @@ func main() {
 	app.Description = "A simple program to benchmark URL responses across multiple requests"
 	app.Usage = "call-it [url] [number of attempts]"
 	app.Version = "0.0.1-beta"
-
-	app.Action = func(c *cli.Context) error {
-		callAttempt, err := call.BuildCall(c.Args(), defaultAttempts, defaultConcurrentCalls)
-		if err != nil {
-			fmt.Println("It was impossible to parse arguments")
-			os.Exit(1)
-		}
-
-		results := callAttempt.MakeIt()
-		call.PrintResults(results)
-		return nil
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "c",
+			Usage: "Pass a config file with all data you want in one or more requests",
+		},
 	}
-
-	err := app.Run(os.Args)
-	if err != nil {
+	app.Action = func(c *cli.Context) (err error) {
+		if c.Bool("c") {
+			return buildCallFromConfig()
+		}
+		return buildCall(c)
+	}
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func buildCall(c *cli.Context) (err error) {
+	callAttempt, err := call.BuildCall(c.Args(), defaultAttempts, defaultConcurrentCalls)
+	if err != nil {
+		return
+	}
+	results := callAttempt.MakeIt()
+	call.PrintResults(results)
+	return
+}
+
+func buildCallFromConfig() (err error) {
+	calls, err := call.BuildCallsFromConfig()
+	if err != nil {
+		return
+	}
+	for _, c := range calls {
+		result := c.MakeIt()
+		call.PrintResults(result)
+	}
+	return
 }
